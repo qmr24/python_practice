855cd81f03590ac7ca590fe114f43e16b04ec28a
Qumair Qwamy <qumairqwamy365@gmail.com>
2026-04-03 11:42:42 +0530

    Add files via upload
    Online Order Management System – Python Practice
    
    This Python program demonstrates a simple order management system using nested data structures.
    
    Features:
    
    Stores customer orders in a dictionary
    Each order contains customer name and a list of items (item name + quantity)
    Find orders by item with case-insensitive search
    Calculate total quantity of each item across all orders
    Demonstrates loops, conditionals, and tuple handling
    
    Purpose:
    This project is part of ICT practice to strengthen skills in Python programming, including nested dictionaries, loops, functions, and basic data processing.

---
+++ Online Order Management System.py
@@ -0,0 +1,34 @@
+orders = {
+    "O1": {
+        "customer": "Ali",
+        "items": [("Pen",2), ("Book",1)]
+    },
+    "O2": {
+        "customer": "Sara",
+        "items": [("Book",3), ("Pencil",5)]
+    }
+}
+
+def find_orders_by_item(orders, target):
+    order_name=[]
+    for key,value in orders.items():
+        for i in orders[key]['items']:
+            if i[0].lower()== target.lower():
+                order_name.append(orders[key]['customer'])
+    return order_name
+
+
+target=input("enter the order item name")
+process =find_orders_by_item(orders, target)
+print(process)
+#assuming that only pen,pencil and book  is available
+total_quantity={'Book':0,'Pencil':0,'Pen':0}
+for key,value in orders.items():
+    for i in orders[key]['items']:
+        if i[0]== 'Book':
+            total_quantity['Book']=int(total_quantity['Book'])+int(i[1])
+        elif i[0]== 'Pen':
+            total_quantity['Pen']=int(total_quantity['Pen'])+int(i[1])
+        elif i[0]== 'Pencil':
+            total_quantity['Pencil']=int(total_quantity['Pencil'])+int(i[1])
+print(total_quantity)
