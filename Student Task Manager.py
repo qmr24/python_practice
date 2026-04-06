a51004eaefa7bd1e0b778f75b3f7cbe43e6473c5
Qumair Qwamy <qumairqwamy365@gmail.com>
2026-04-06 21:29:27 +0530

    Add files via upload

--- Student Task Manager.py
+++ Student Task Manager.py
@@ -1,16 +1,55 @@
 task={}
 task_id_count=1
+#adding task
 def add_task():
-    global task_id_count
-    task_id_countf="T"+str(task_id_count).zfill(3)
-    print(("="*15))
+    priority=["high","medium","low"]
+    while True:
+        global task_id_count
+        task_id_countf="T"+str(task_id_count).zfill(3)
+        print(("="*20))
+        global task
+        task_id=task_id_countf
+        task_title=input("ENTER THE TASK TITLE")
+        while True:
+            task_priority=input("ENTER THE TASK PRIORITY (High / Medium / Low))")
+            if task_priority.lower() not in priority:
+                print('enter a vaild priority value (High / Medium / Low))')
+                continue
+            break
+        task[task_id]={"TITLE":task_title ,"IMPORTANT":task_priority}
+        task_id_count=int(task_id_count)+1
+        ask=input("do you want to add more (yes/no)")
+        if ask.lower()=="no":
+            break
+        elif ask.lower()=="yes":
+            continue
+        else:
+            print("enter correct input")
+
+#display task
+def display_tasks():
     global task
-    task_id=task_id_countf
-    task_title=input("ENTER THE TASK TITLE")
-    task_priority=input("ENTER THE TASK PRIORITY (High / Medium / Low))")
-    task[task_id]={"TASK ID":task_id ,"TITLE":task_title}
-    task_id_count=int(task_id_count)+1
+    for key,value in task.items():
+        print(f"{key} ==> {task[key]['TITLE']} ==> {task[key]['IMPORTANT']}")
+
+#search task
+def search_task():
+    result=[]
+    task_search=input("enter the name of the task to be searched")
+    for key,value in task.items():
+        if task_search.lower()== task[key]["TITLE"].lower():
+            result.append(task[key])
+    print(result)
+
+#del task[task_id]
+            
+
+
+
+
 add_task()
-print(task)
-    
+display_tasks()
+search_task()
+
+
 
